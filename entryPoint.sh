33ffa373f92bdccdcb7f2546a8d74de1d0dbd40b
Kartikey Raut <37393924+kartikeyraut@users.noreply.github.com>
2025-01-05 09:37:29 +0000

    creating docker file

---
+++ entryPoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "================="
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+python3
+
+echo "================="

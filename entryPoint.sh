1a07ec33f4850dfd51019d007574476e75d9d5ba
Kartikey Raut <37393924+kartikeyraut@users.noreply.github.com>
2025-01-05 10:01:29 +0000

    updated entrypoint.sh

--- entryPoint.sh
+++ entryPoint.sh
@@ -6,6 +6,10 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3
+node /app/feed.js
+
+git add *
+git commit -m "Updated Feed"
+git push --set-upstream origin main
 
 echo "================="
